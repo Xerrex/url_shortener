1fdf12167fa6ac6559bdfc19e9402640d3cf1c15
Alex Kagai <alexkagai@outlook.com>
2021-11-18 15:39:12 +0300

    Add the Home page

--- shortener/views.py
+++ shortener/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
     """Takes user input for url to shorten
     """
-    return HttpResponse("welcome home")
+    return render(request, "shortener/home.html")
